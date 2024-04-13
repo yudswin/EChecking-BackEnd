d5bf146b96bf0804dd71b552ebe90ea746d755ff
ChanDenBlue <146571822+ChanDenBlue@users.noreply.github.com>
2024-04-13 21:43:03 +0700

    Add files via upload

---
+++ src/controllers/Studentcontroller.js
@@ -0,0 +1,68 @@
+const LecturerService = require('../services/StudentService')
+const JwtService = require('../services/JwtService')
+
+const loginStudent = async (req, res) => {
+    try {
+        const { name, email, password, confirmPassword, phone } = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if (!email || !password || !confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirmPassword'
+            })
+        }
+        const response = await StudentService.loginStudent(req.body)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+const updateStudent = async (req, res) => {
+    try {
+        const studentID = req.params.id
+        const data = req.body
+        if (!studentID) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The studentID is required'
+            })
+        }        
+        const response = await StudentService.updateStudent(studentID, data)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+const logoutStudent = async (req, res) => {
+    try {
+        res.clearCookie('refresh_token')
+        return res.status(200).json({
+            status: 'OK',
+            message: 'Logout successfully'
+        })
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+module.exports = {
+    loginStudent,
+    updateStudent,
+    logoutStudent
+}
