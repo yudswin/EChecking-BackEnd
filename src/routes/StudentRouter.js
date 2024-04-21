aae429723e1f1f90f16248d6592ee34cb4a90cdb
ChanDenBlue <146571822+ChanDenBlue@users.noreply.github.com>
2024-04-21 22:22:21 +0700

    Update StudentRouter.js
    update router

--- src/routes/StudentRouter.js
+++ src/routes/StudentRouter.js
@@ -4,7 +4,7 @@ const lecturerController = require('../controllers/Studentcontroller');
 const { authMiddleWare, authUserMiddleWare } = require("../middleware/authMiddleware");
 
 // CRUD - Create Review Update Delete
-router.post('/sign-in', Studentontroller.loginStudent) //  login of Student
+router.post('/sign-in', Studentcontroller.loginStudent) //  login of Student
 router.put('/update user/:id',authUserMiddleWare, Studentcontroller.updateStudent)
 router.post('/login', Studentcontroller.logoutStudent);
 
