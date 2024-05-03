d5f5793b8604d8ac9ee8219da8f3c9649d5febc5
ChanDenBlue <146571822+ChanDenBlue@users.noreply.github.com>
2024-05-03 21:56:19 +0700

    Update Studentcontroller.js fix 2

--- src/controllers/Studentcontroller.js
+++ src/controllers/Studentcontroller.js
@@ -1,6 +1,54 @@
 const StudentService = require('../services/StudentService')
 const JwtService = require('../services/JwtService')
 
+const createStudent = async (req, res) => {
+    try {
+        const { firstName, lastName, studentID, phone, studentPassword, confirmPassword, email } = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if (!email || !studentPassword || !confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        } else if (studentPassword !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirmPassword'
+            })
+        }
+    //    console.log(req.body)
+        const response = await StudentService.createStudent(req.body)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+const refreshToken = async (req, res) =>{
+    try{
+        const refresh_token = req.headers.token.split(' ')[1]       // create Beare in Headers of Postman  (put the refresh token to headers  => verify refresh token => ))
+        if(!refresh_token){
+            return res.status(200).json({
+                status: "ERROR",
+                msg: "The refresh token is required"
+            })
+        }
+
+        const response = await JwtService.refreshTokenJwtService(refresh_token)
+        return res.status(200).json(response)
+    }catch(error){
+        return res.status(404).json({
+            mgs: error
+        })
+    }
+} 
 const loginStudent = async (req, res) => {
     try {
         const { studentID, password } = req.body
@@ -57,7 +105,9 @@ const logoutStudent = async (req, res) => {
     }
 }
 module.exports = {
+    createStudent,
     loginStudent,
     updateStudent,
+    refreshToken,
     logoutStudent
 }
