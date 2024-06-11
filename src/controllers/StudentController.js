38f59a38634a065ace2c2e27ceb17276b5214cfb
NguyenBaPhucITITIU20278 <91875515+NguyenBaPhucITITIU20278@users.noreply.github.com>
2024-06-12 00:16:23 +0700

    forgotPassword

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,177 +1,210 @@
-const Student = require('../models/StudentModel')
-const StudentService = require('../services/StudentService')
-const JwtService = require('../services/JwtService')
-
+const Student = require("../models/StudentModel");
+const StudentService = require("../services/StudentService");
+const JwtService = require("../services/JwtService");
 
 const createStudent = async (req, res) => {
-    try {
-        const { firstName, lastName, studentID, phone, password, confirmPassword, email} = req.body
-        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-        const isCheckEmail = reg.test(email)
-        if (!email || !password || !confirmPassword) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        } else if (!isCheckEmail) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is email'
-            })
-        } else if (password !== confirmPassword) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The password is equal confirmPassword'
-            })
-        }
+  try {
+    const {
+      firstName,
+      lastName,
+      studentID,
+      phone,
+      password,
+      confirmPassword,
+      email,
+    } = req.body;
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isCheckEmail = reg.test(email);
+    if (!email || !password || !confirmPassword) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is required",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is email",
+      });
+    } else if (password !== confirmPassword) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The password is equal confirmPassword",
+      });
+    }
     //    console.log(req.body)
-        const response = await StudentService.createStudent(req.body)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+    const response = await StudentService.createStudent(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+const loginStudent = async (req, res) => {
+  try {
+    const { studentID, password } = req.body;
+    //   console.log(req.body)
+    if (!password) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is required",
+      });
+    } else if (!studentID) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is ID",
+      });
     }
-}
 
-const loginStudent = async (req, res) =>{
-    try{
-        const { studentID, password } = req.body
-     //   console.log(req.body)
-        if (!password) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        } else if (!studentID) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is ID'
-            })
-        }
+    const response = await StudentService.loginStudent(req.body);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(404).json({
+      mgs: error,
+    });
+  }
+};
 
-        const response = await StudentService.loginStudent(req.body)
-        return res.status(200).json(response)
-    }catch(error){
-        return res.status(404).json({
-            mgs: error
-        })
+const updateStudent = async (req, res) => {
+  try {
+    const StudentId = req.params.id;
+    const data = req.body;
+    if (!StudentId) {
+      return res.status(200).json({
+        status: "ERROR",
+        msg: "The studentID is required",
+      });
     }
-} 
 
+    const response = await StudentService.updateStudent(StudentId, data);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(404).json({
+      mgs: error,
+    });
+  }
+};
 
-const updateStudent = async (req, res) =>{
-    try{
-        const StudentId = req.params.id
-        const data = req.body
-        if(!StudentId){
-            return res.status(200).json({
-                status: "ERROR",
-                msg: "The studentID is required"
-            })
-        }
-
-        const response = await StudentService.updateStudent(StudentId, data)
-        return res.status(200).json(response)
-    }catch(error){
-        return res.status(404).json({
-            mgs: error
-        })
+const refreshToken = async (req, res) => {
+  try {
+    const refreshToken = req.headers.token.split(" ")[1]; // create Beare in Headers of Postman  (put the refresh token to headers  => verify refresh token => ))
+    if (!refreshToken) {
+      return res.status(200).json({
+        status: "ERROR",
+        msg: "The refresh token is required",
+      });
     }
-} 
 
-
-const refreshToken = async (req, res) =>{
-    try{
-        const refreshToken = req.headers.token.split(' ')[1]       // create Beare in Headers of Postman  (put the refresh token to headers  => verify refresh token => ))
-        if(!refreshToken){
-            return res.status(200).json({
-                status: "ERROR",
-                msg: "The refresh token is required"
-            })
-        }
-
-        const response = await JwtService.refreshTokenJwtService(refreshToken)
-        return res.status(200).json(response)
-    }catch(error){
-        return res.status(404).json({
-            mgs: error
-        })
-    }
-} 
-
+    const response = await JwtService.refreshTokenJwtService(refreshToken);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(404).json({
+      mgs: error,
+    });
+  }
+};
 
 const logoutStudent = async (req, res) => {
-    try {
-        res.clearCookie('refreshToken')
-        return res.status(200).json({
-            status: 'OK',
-            message: 'Logout successfully'
-        })
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    res.clearCookie("refreshToken");
+    return res.status(200).json({
+      status: "OK",
+      message: "Logout successfully",
+    });
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getDetails = async (req, res) => {
-    try {
-        const studentId = req.params.id
-        if (!studentId) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The studentId is required'
-            })
-        }
-        const response = await StudentService.getDetails(studentId)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const studentId = req.params.id;
+    if (!studentId) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The studentId is required",
+      });
     }
-}
+    const response = await StudentService.getDetails(studentId);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getAllStudents = async (req, res) => {
-    try {
-        const response = await StudentService.getAllStudents()
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    const response = await StudentService.getAllStudents();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 const forgotPassword = async (req, res) => {
-    try {
-        const { email } = req.body;
-        if (!email) {
-            return res.status(400).json({
-                status: 'ERR',
-                message: 'Email is required'
-            });
-        }
-
-        const student = await StudentService.findStudentByEmail(email);
-        const response = await StudentService.sendOTP(email);
-        
-        return res.status(200).json(response);
-    } catch (error) {
-        return res.status(500).json({
-            status: 'ERR',
-            message: error.message
-        });
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "Email is required",
+      });
     }
-}
 
-module.exports = {
-    createStudent, 
-    loginStudent, 
-    updateStudent,
-    refreshToken,
-    logoutStudent,
-    getDetails,
-    getAllStudents,
-    forgotPassword
+    const student = await StudentService.findStudentByEmail(email);
+    const response = await StudentService.sendOTP(email);
+
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      status: "ERR",
+      message: error.message,
+    });
+  }
+};
+const verifyOtp = async (req, res) => {
+  const { email, otp } = req.body;
+  if (!email || !otp) {
+    return res.status(400).json({
+      status: "ERR",
+      message: "Email and OTP are required.",
+    });
+  }
+  else{
+    return res.status(200).json({
+      status: "OK",
+      message: "OTP is verified.",
+    });
+  }
+};
+const changePassword = async (req, res) => {
+  const { email, otp, newPassword } = req.body;
+  if (!email || !otp || !newPassword) {
+    return res.status(400).json({
+      status: "ERR",
+      message: "Email, OTP and new password are required.",
+    });
+  }
+  else{
+    const response = await StudentService.changePassword(email, otp, newPassword);
+    return res.status(200).json(response);
+  }
 }
+module.exports = {
+  createStudent,
+  loginStudent,
+  updateStudent,
+  refreshToken,
+  logoutStudent,
+  getDetails,
+  getAllStudents,
+  forgotPassword,
+  verifyOtp,
+  changePassword,
+};
--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -1,264 +1,316 @@
-const Student = require("../models/StudentModel")
+const Student = require("../models/StudentModel");
 const nodemailer = require("nodemailer");
-const OTP = require("../models/OTPModel")
+const OTP = require("../models/OTPModel");
 // const User = require("../models/StudentModel.js")
-const bcrypt = require("bcrypt")
-const { generalAccessToken, generalRefreshToken } = require("./JwtService.js")
-
-
-
+const bcrypt = require("bcrypt");
+const { generalAccessToken, generalRefreshToken } = require("./JwtService.js");
 
 const createStudent = (newStudent) => {
-    return new Promise(async (resolve, reject) => {
-        const { firstName, lastName, studentID, phone, password, email } = newStudent
-        try {
-            const checkStudent = await Student.findOne({
-                email: email
-            })
-            if (checkStudent !== null) {
-                resolve({
-                    status: 'ERR',
-                    message: 'The email is already'
-                })
-            }
-            const hash = bcrypt.hashSync(password, 10)
-            //   console.log(hash)
-            const createdStudent = await Student.create({
-                firstName,
-                lastName,
-                studentID,
-                phone,
-                password: hash,
-                email
-
-
-            })
-            if (createdStudent) {
-                resolve({
-                    status: 'OK',
-                    message: 'SUCCESS',
-                    data: createdStudent
-                })
-            }
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
-
-
-const loginStudent = (StudentLogin) => {
-    return new Promise(async (resolve, reject) => {
-        const { studentID, password } = StudentLogin
-        //   console.login
-        try {
-            const checkStudent = await Student.findOne({
-                studentID: studentID
-            })
-            if (checkStudent === null) {
-                resolve({
-                    status: "Error",
-                    message: "The student is not defined",
-                })
-            }
-            const comparePassword = bcrypt.compareSync(password, checkStudent.password)
-            //    console.log(comparePassword)
-
-            if (!comparePassword) {
-                resolve({
-                    status: "Error",
-                    message: "Password or student is incorrect",
-                })
-            }
-            const accessToken = await generalAccessToken({
-                id: checkStudent.id,
-            })
-
-            const refreshToken = await generalRefreshToken({ // when access token is expired => provide the new access_token
-                id: checkStudent.id,
-            })
-
-            //    console.log(access_token)
-            resolve({
-                status: "OK",
-                message: "SIGN-IN SUCCESS",
-                accessToken,
-                refreshToken,
-            })
-
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
-
-const updateStudent = (id, data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const checkStudent = await Student.findOne({
-                _id: id
-            })
-
-            if (checkStudent === null) {
-                resolve({
-                    status: "Error",
-                    mgs: "The student is not defined"
-                })
-            }
-
-            const updatedStudent = await Student.findByIdAndUpdate(id, data, { new: true })
-            resolve({
-                status: "OK",
-                message: "SUCCESS",
-                data: updatedStudent
-            })
-
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
-
-const getDetails = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const checkStudent = await Student.findOne({
-                _id: id
-            })
-            if (checkStudent === null) {
-                resolve({
-                    status: "Error",
-                    mgs: "The student is not defined"
-                })
-            }
-            resolve({
-                status: "OK",
-                message: "SUCCESS",
-                data: checkStudent
-            })
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
-
-const getAllStudents = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const students = await Student.find({})
-            if (students === null) {
-                resolve({
-                    status: "Error",
-                    mgs: "The student is not defined"
-                })
-            }
-            resolve({
-                status: "OK",
-                message: "SUCCESS",
-                data: students
-            })
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
-const findStudentByEmail = (email) => {
-    return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const { firstName, lastName, studentID, phone, password, email } =
+      newStudent;
     try {
-        const student = await Student.findOne({ email: email });
-        if(student === null){
-            resolve({
-                status: "Error",
-                message: "The student is not defined"
-            })
-        }
-        else{
-            resolve({
-                status: "OK",
-                message: "SUCCESS",
-                data: student
-            })
-        }
-    } catch (error) {
-        reject(error)
+      const checkStudent = await Student.findOne({
+        email: email,
+      });
+      if (checkStudent !== null) {
+        resolve({
+          status: "ERR",
+          message: "The email is already",
+        });
+      }
+      const hash = bcrypt.hashSync(password, 10);
+      //   console.log(hash)
+      const createdStudent = await Student.create({
+        firstName,
+        lastName,
+        studentID,
+        phone,
+        password: hash,
+        email,
+      });
+      if (createdStudent) {
+        resolve({
+          status: "OK",
+          message: "SUCCESS",
+          data: createdStudent,
+        });
+      }
+    } catch (e) {
+      reject(e);
     }
-    })
-}
-function generateRandomString(length) {
-    const characters = '0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
-const sendOTP = (email) => {
-return new Promise(async (resolve, reject) => {
-    const otpCode = generateRandomString(4)
-    console.log(otpCode)
-    try {
-        const createdOTP = await OTP.create({
-          otp: otpCode,
-          email
-        })
-        if(createdOTP){
-            const info = await transporter.sendMail(mailOption,function(err,info)   {
-                if(err){
-                    console.log(err)
-                }
-                else{
-                    console.log("Email sent:" +info.response)
-                }
-            })
-            if(info){
-                resolve({
-                    status: "OK",
-                    message: "SUCCESS",
-                    data: info
-                })
-            }
-            else{
-                resolve({
-                    status: "Error",
-                    message: "The OTP is not created"
-                })
-            }
-               
-        }
-        else{
-            resolve({
-                status: "Error",
-                message: "The OTP is not created"
-            })
-        }
-    } catch (error) {
-        reject(error)
-    }
-})
-}
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    secure: false,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
   });
-const mailOption = {
-    from: process.env.EMAIL, // sender address
-    to: "nqd5758@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
 };
 
-module.exports = {
-    createStudent,
-    loginStudent,
-    updateStudent,
-    getDetails,
-    getAllStudents,
-    findStudentByEmail,
-    sendOTP,
-    generateRandomString,
-  
+const loginStudent = (StudentLogin) => {
+  return new Promise(async (resolve, reject) => {
+    const { studentID, password } = StudentLogin;
+    //   console.login
+    try {
+      const checkStudent = await Student.findOne({
+        studentID: studentID,
+      });
+      if (checkStudent === null) {
+        resolve({
+          status: "Error",
+          message: "The student is not defined",
+        });
+      }
+      const comparePassword = bcrypt.compareSync(
+        password,
+        checkStudent.password
+      );
+      //    console.log(comparePassword)
+
+      if (!comparePassword) {
+        resolve({
+          status: "Error",
+          message: "Password or student is incorrect",
+        });
+      }
+      const accessToken = await generalAccessToken({
+        id: checkStudent.id,
+      });
+
+      const refreshToken = await generalRefreshToken({
+        // when access token is expired => provide the new access_token
+        id: checkStudent.id,
+      });
+
+      //    console.log(access_token)
+      resolve({
+        status: "OK",
+        message: "SIGN-IN SUCCESS",
+        accessToken,
+        refreshToken,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const updateStudent = (id, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkStudent = await Student.findOne({
+        _id: id,
+      });
+
+      if (checkStudent === null) {
+        resolve({
+          status: "Error",
+          mgs: "The student is not defined",
+        });
+      }
+
+      const updatedStudent = await Student.findByIdAndUpdate(id, data, {
+        new: true,
+      });
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        data: updatedStudent,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const getDetails = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkStudent = await Student.findOne({
+        _id: id,
+      });
+      if (checkStudent === null) {
+        resolve({
+          status: "Error",
+          mgs: "The student is not defined",
+        });
+      }
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        data: checkStudent,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const getAllStudents = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const students = await Student.find({});
+      if (students === null) {
+        resolve({
+          status: "Error",
+          mgs: "The student is not defined",
+        });
+      }
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        data: students,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+const findStudentByEmail = (email) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const student = await Student.findOne({ email: email });
+      if (student === null) {
+        resolve({
+          status: "Error",
+          message: "The student is not defined",
+        });
+      } else {
+        resolve({
+          status: "OK",
+          message: "SUCCESS",
+          data: student,
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+function generateRandomString(length) {
+  const characters = "0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
+const sendOTP = (email) => {
+  return new Promise(async (resolve, reject) => {
+    const otpCode = generateRandomString(4);
+    console.log("Generated OTP:", otpCode);
+    try {
+      const createdOTP = await OTP.create({
+        otp: otpCode,
+        email,
+      });
+      if (createdOTP) {
+        const mailOptions = {
+          from: process.env.EMAIL,
+          to: email,
+          subject: "Reset Password",
+          html: `<p>
+            Your OTP code is: ${otpCode}
+            <p">This link will expire in 10 minutes.</p>`,
+        };
+        const info = await transporter.sendMail(
+          mailOptions,
+          function (err, info) {
+            if (err) {
+              console.log(err);
+              reject({
+                status: "Error",
+                message: "Failed to send email",
+                error: err,
+              });
+            } else {
+              console.log("Email sent:" + info.response);
+              resolve({
+                status: "OK",
+                message: "SUCCESS",
+                data: info,
+              });
+            }
+          }
+        );
+      } else {
+        resolve({
+          status: "Error",
+          message: "The OTP is not created",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      reject({
+        status: "Error",
+        message: "Error in OTP creation or email sending",
+        error: error,
+      });
+    }
+  });
+};
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+// const mailOption = {
+//     from: process.env.EMAIL, // sender address
+//     to: ["phucnguyenba582002@gmail.com"], // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello", // plain text body
+// };
+const changePassword = async (email, otp, newPassword) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const otpRecord = await OTP.findOne({  otp: otp });
+      if (!otpRecord) {
+        resolve({
+          status: "Error",
+          message: "Invalid OTP or OTP has expired",
+          data: otpRecord
+        });
+        return;
+      }
+
+      const hashedPassword = bcrypt.hashSync(newPassword, 10);
+      const updatedStudent = await Student.findOneAndUpdate(
+        { email: email },
+        { password: hashedPassword },
+        { new: true }
+      );
+      if (!updatedStudent) {
+        resolve({
+          status: "Error",
+          message: "Student not found",
+        });
+        return;
+      }
+      await OTP.deleteOne({ email: email, otp: otp });
+
+      resolve({
+        status: "OK",
+        message: "Password updated successfully",
+        data: updatedStudent,
+      });
+    } catch (error) {
+      reject({
+        status: "Error",
+        message: "Failed to update password",
+        error: error,
+      });
+    }
+  }).catch((error) => {
+    console.error("Error in changePassword:", error);
+    // Additional error handling or logging here
+  });
+};
+module.exports = {
+  createStudent,
+  loginStudent,
+  updateStudent,
+  getDetails,
+  getAllStudents,
+  findStudentByEmail,
+  sendOTP,
+  generateRandomString,
+  changePassword,
+};
